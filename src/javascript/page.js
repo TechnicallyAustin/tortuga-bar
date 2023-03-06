43fecc3c3e29dc0489f319b88a208b559400abd2
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-03-05 22:26:24 -0800

    working on deesign, page creation complete, need to add meneu and more styles

--- src/javascript/page.js
+++ src/javascript/page.js
@@ -1,67 +1,82 @@
 export const page = {
-  content: function(){
-    const content = document.querySelector(".content")
-    this.content = content
+  content: function () {
+    const content = document.querySelector(".content");
+    this.content = content;
   },
-  header: function(){
+  header: function () {
     const header = content.appendChild(document.createElement("header"));
-    header.setAttribute("class", "header bg-warning")
+    header.setAttribute("class", "header position-sticky top-0 bg-opacity-50");
   },
-  navbar: function(){
-    const headerSelector = document.querySelector(".header")
-    const navbar = headerSelector.appendChild(document.createElement("navbar"));
-    navbar.setAttribute("class", "navbar");
-  },
-  navItems: function(){
-    const nav = document.querySelector(".navbar");
+  navbar: function () {
+    const headerSelector = document.querySelector(".header");
+    const nav = headerSelector.appendChild(document.createElement("navbar"));
+    nav.setAttribute("class", "navbar d-flex justify-content-between align-items-center");
 
-    function title(){
-      const container = nav.appendChild(document.createElement("div"));
-      const title = container.appendChild(document.createElement("p"));
-      title.setAttribute("class", "nav-title");
-      title.textContent = "Tech Bar"
-    };
-    title()
+    const items = {
+      title: function () {
+        const container = nav.appendChild(document.createElement("div"));
+        const title = container.appendChild(document.createElement("p"));
+        title.setAttribute("class", "nav-title fs-2 mb-0 ms-3");
+        title.textContent = "Tech Bar IO";
+      },
+      list: function () {
+        const list = nav.appendChild(document.createElement("ul"));
+        list.setAttribute("class", "nav");
 
-    function list(){
-      const list = nav.appendChild(document.createElement("ul"))
-      list.setAttribute("class", "");
-
-      function listItems(){
-        const links = ["About", "Menu", "Drinks"]
-        for (let link of links){
-          let item = list.appendChild(document.createElement("li"))
-          item.setAttribute("class","");
-          item.textContent = link
+        const links = ["About", "Menu", "Drinks"];
+        for (let link of links) {
+          let item = list.appendChild(document.createElement("li"));
+          let itemLink = item.appendChild(document.createElement("a"))
+          item.setAttribute("class", "nav item");
+          itemLink.setAttribute("class", "nav-link")
+          itemLink.textContent = link;
         }
-      }; listItems()
-    }; list()
+      },
+    };
+    items.title();
+    items.list();
   },
-  main: function(){
+  main: function () {
     const main = content.appendChild(document.createElement("main"));
-    main.setAttribute("class", "main bg-dark")
-    
+    main.setAttribute("class", "main bg-dark");
+
     const elements = {
-      hero: function(){
+      hero: function () {
         const hero = main.appendChild(document.createElement("div"));
         hero.setAttribute("class", "");
-      },
-      section: function(){
-        const section = main.appendChild(document.createElement("section"));
-        section.setAttribute("class","");
-      }
-    }
 
+        const heroItems = {
+          img: function () {
+            const img = hero.appendChild(document.createElement("img"));
+            img.setAttribute("class", "img-fluid");
+            img.setAttribute("src", "../src/images/bar-2.jpg");
+            img.setAttribute("alt", "");
+          },
+          title: function () {
+            const title = hero.appendChild(document.createElement("h1"));
+            title.setAttribute("class", "");
+            title.textContent = "Tehcnically a Bar";
+          },
+        };
+        heroItems.img();
+        heroItems.title();
+      },
+      section: function () {
+        const section = main.appendChild(document.createElement("section"));
+        section.setAttribute("class", "");
+      },
+    };
+    elements.hero();
+    elements.section();
   },
-  footer: function(){
+  footer: function () {
     const footer = content.appendChild(document.createElement("footer"));
     footer.setAttribute("class", "footer bg-warning");
   },
-  build: function(){
-    this.header()
-    this.navbar()
-    this.navItems()
-    this.main()
-    this.footer()
-  }
-}
+  build: function () {
+    this.header();
+    this.navbar();
+    this.main();
+    this.footer();
+  },
+};
