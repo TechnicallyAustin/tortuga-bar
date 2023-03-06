8a0d58c0a5d4c7634290e339000eec99af01e5f2
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-03-05 18:04:32 -0800

    more styling changes

--- src/javascript/page.js
+++ src/javascript/page.js
@@ -1,23 +1,23 @@
-  //creates the API for the page
+//creates the API for the page
 
-  export function newPage() {
+export function newPage() {
   const content = document.body.appendChild(document.createElement("div"));
   content.setAttribute("class", "content bg-dark");
 
   // Header
   const header = content.appendChild(document.createElement("header"));
-  header.setAttribute("class", "header col-12 bg-info");
+  header.setAttribute("class", "header col-12");
 
   // nav
   const nav = header.appendChild(document.createElement("nav"));
   nav.setAttribute(
     "class",
-    "nav col-12 bg-warning d-flex justify-content-around align-items-center"
+    "nav col-12  position-sticky top-0 bg-transparent d-flex justify-content-around align-items-center"
   );
 
   // nav title
   const title = nav.appendChild(document.createElement("h1"));
-  title.setAttribute("class", "title nav-item col-5 ");
+  title.setAttribute("class", "title nav-item text-white col-5 ");
   title.textContent = "Technically Bar";
 
   // nav list
@@ -38,23 +38,28 @@
     link.setAttribute("href", "#");
     link.textContent = item;
   }
-  // main tab
+  // main
   const main = content.appendChild(document.createElement("main"));
-  main.setAttribute("class", "main bg-dark d-flex flex-column justify-content-around align-items-center");
-
+  main.setAttribute(
+    "class",
+    "main bg-dark d-flex flex-column justify-content-around align-items-center"
+  );
+  //hero
   const hero = main.appendChild(document.createElement("div"));
-  hero.setAttribute("class", "hero bg-light  h-25 w-100");
+  hero.setAttribute("class", "hero carousel carousel-dark bg-dark  h-50 w-100");
 
-  const sectionTitle = hero.appendChild(document.createElement("h2"));
-  sectionTitle.setAttribute("class", "w-100 bg-light")
-  sectionTitle.textContent = "Section Title"
+  const heroImg = hero.appendChild(document.createElement("img"));
+  heroImg.setAttribute("class","hero-image img-fluid z-index-1")
+  heroImg.setAttribute("src", "../src/images/bar.jpg");
 
+  const caption = hero.appendChild(document.createElement("h2"));
+  caption.setAttribute("class", "carousel-caption text-white bg-dark bg-opacity-50  position-absolute h-25 top-0 bg- fs-1 m-0 p-0 ");
+  caption.textContent = "Technically the Best Bar Ever.";
 
-
+  // tabs
   const section = main.appendChild(document.createElement("section"));
-  section.setAttribute("class", "section-tab h-50 w-75 bg-light");
+  section.setAttribute("class", "section-tab h-75 w-100 bg-light");
 
   const footer = content.appendChild(document.createElement("footer"));
   footer.setAttribute("class", "footer  h-100 w-100 bg-warning");
 }
-
